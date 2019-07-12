97211b5e55653644790dd2a05799c6fe2af67eed
SierraOG <sierrao@utexas.edu>
2019-07-12 10:33:49 -0500

    finished tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,37 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+// tabs will be appended to this element
+const tabsElement = document.querySelector('.topics');
+
+// axios get request
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(data => {
+    console.log(data)
+    // make array of topics 
+    const topicsArray = data.data.topics
+    // making a new tab for each topic sin topics array and appending it to tabsElement
+    topicsArray.forEach(topic => {
+    let element = createTab(topic)
+    tabsElement.appendChild(element)
+    })
+})
+.catch(error => {
+    console.log('error')
+})
+
+// tab creator function
+function createTab(subject){
+    // create new element
+    const tabDiv = document.createElement('div');
+
+    // add class
+    tabDiv.classList.add('tab');
+
+    // add text content
+    tabDiv.textContent = subject;
+
+    // return tab 
+    return tabDiv;
+}
