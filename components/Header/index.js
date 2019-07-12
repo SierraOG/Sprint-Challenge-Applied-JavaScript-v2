694d18cd5a91d8a232d1a823c7f11224ce97600f
SierraOG <sierrao@utexas.edu>
2019-07-12 10:19:35 -0500

    finished header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,32 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+    // create new elements 
+    const headerDiv = document.createElement('div');
+    const date = document.createElement('span');
+    const header = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    // add classes 
+    headerDiv.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    // add text content
+    date.textContent = 'SMARCH 28, 2019';
+    header.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    // set up structure
+    headerDiv.appendChild(date);
+    headerDiv.appendChild(header);
+    headerDiv.appendChild(temp);
+
+    // return component
+    return headerDiv;
+}
+
+const headerContainer = document.querySelector('.header-container');
+const newHeader = Header();
+headerContainer.append(newHeader);
